d097ae7e85d92c7643c588548f24f5e043726859
KoshkaKsu <ksenia.kuzn@mail.ru>
2021-03-31 00:20:08 +0300

    feat:добавить возможность добавления новой карточки

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 let popup = document.querySelector('.popup');
-let popupForm = popup.querySelector('.popup__container');
-let popupPlace = document.querySelector('.popup__place');
+let popupForm = popup.querySelector('#popup-edit');
 let popupEditOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = popup.querySelector('.popup__button-close');
 let popupSaveButton = popup.querySelector('.popup__button-save');
@@ -9,6 +8,7 @@ let profileJob = document.querySelector('.profile__job');
 let nameInput = popup.querySelector('.popup__profile_name_name');
 let jobInput = popup.querySelector('.popup__profile_name_job');
 
+let popupPlace = document.querySelector('.popup__place');
 let popupAddButton = document.querySelector('.profile__add-button');
 let photos = document.querySelector('.photos');
 let namePlace = popupPlace.querySelector('.popup__profile_name_title');
@@ -17,6 +17,8 @@ let popupAddCloseButton = popupPlace.querySelector('.popup__button-close');
 
 let templatePhoto = document.querySelector('#photo-template').content.querySelector('.grid-item');
 let photosList = document.querySelector('.elements');
+let titleInput = document.querySelector('.popup__profile_name_title');
+let photoInput = document.querySelector('.popup__profile_name_photo');
 
 const initialCards = [
     {
@@ -57,8 +59,8 @@ initialCards.forEach( elem => {
     cardsElement.querySelector('.grid-item__name').textContent = elem.name;
     cardsElement.querySelector('.grid-item__photo').src = elem.link;
 
-    cardsElement.querySelector('.grid-item__like').addEventListener('click', function (event) {
-      event.target.classList.toggle('grid-item__like_active');
+    cardsElement.querySelector('.grid-item__like').addEventListener('click', function (evt) {
+      evt.target.classList.toggle('grid-item__like_active');
     });
 
     photosList.append(cardsElement);
@@ -74,13 +76,6 @@ function closeButton () {
     popup.classList.toggle('popup_opened');
 }
 
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value; 
-    closeButton();
-}
-
 function addCard () {
     namePlace.value = '';
     linkPlace.value = '';
@@ -91,8 +86,26 @@ function closeAddButton () {
     popupPlace.classList.toggle('popup_opened');
 }
 
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value; 
+    closeButton();
+}
+
+function cardsSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы
+    let cardsElement = templatePhoto.cloneNode(true);
+    cardsElement.querySelector('.grid-item__name').textContent = titleInput.value;
+    cardsElement.querySelector('.grid-item__photo').src = photoInput.value;
+    closeAddButton();
+    photosList.prepend(cardsElement);
+}
+
 popupEditOpenButton.addEventListener('click', editOpenButton);
 popupCloseButton.addEventListener('click', closeButton);
-popupForm.addEventListener('submit', formSubmitHandler);
 popupAddButton.addEventListener('click', addCard);
 popupAddCloseButton.addEventListener('click', closeAddButton);
+
+popupForm.addEventListener('submit', formSubmitHandler);
+popupPlace.addEventListener('submit', cardsSubmitHandler);
